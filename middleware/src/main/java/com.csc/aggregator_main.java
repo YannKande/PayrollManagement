94a5bf3a040010af6fd0ca5e7f89dbbd3579d705
yankande <info@yannickkande.tech>
2023-06-07 11:36:49 +0400

    git(pushin): add remote project

---
+++ middleware/src/main/java/com.csc/aggregator_main.java
@@ -0,0 +1,12 @@
+package com.csc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class aggregator_main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(aggregator_main.class, args);
+    }
+}
